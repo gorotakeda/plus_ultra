419d8c093de073ee3eec0c81125c370ffe7d944c
gorotakeda <rugbytkg3@gmail.com>
2021-08-24 19:32:44 +0900

    postモデルの単体テストコード

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :post do
-    
+    title      { Faker::Lorem.sentence }
+    meal       { Faker::Lorem.sentence }
+    workout    { Faker::Lorem.sentence }
+    sex_id     { 1 }
+    age_id     { 1 }
+    style_id   { 1 }
+    purpose_id { 1 }
+    association :user
   end
 end
